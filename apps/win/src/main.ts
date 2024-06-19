80423e987a5206e79e6640045d66aeeac2da89ca
Z <zhenisbusy@qq.com>
2024-06-19 23:30:37 +0800

    modify layout

--- apps/win/src/main.ts
+++ apps/win/src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from 'layout'
 import 'virtual:uno.css'
 
 const app = createApp(App)
--- composite/layout/src/Layout.vue
+++ composite/layout/src/Layout.vue
@@ -1,9 +1,23 @@
 <script lang="ts" setup>
+import { ref } from 'vue'
+import { ElDialog, ElIcon } from 'element-plus'
 import WindowBar from './window-bar/WindowBar.vue'
+
+const isDialogShow = ref(false)
 </script>
 
 <template>
   <WindowBar />
+  <ElDialog
+    v-model="isDialogShow"
+    :modal="false"
+    append-to-body
+    :lock-scroll="false"
+    show-close
+    draggable
+  >
+    <component :is="dialogContent" />
+  </ElDialog>
 </template>
 
 <style lang="scss"></style>
